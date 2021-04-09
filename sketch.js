9d167c08545001d5b5c94df09aa3e2ce44d594bc
Naimish <naimishsudheer@gmail.com>
2021-04-09 15:58:06 +0530

    c46

--- sketch.js
+++ sketch.js
@@ -1,23 +1,39 @@
 var bulletGroup,floatingObjectGroup,cloudGroup
 var mortar
-var mortarImage
+var mortarImageback
 var cloudImage
+var backgroundImage
 
 function preload (){
-  mortarImage=loadImage("Images/tile000.png")
+  mortarImage=loadImage("Images/cannon.png")
   cloudImage=loadImage("Images/cloud.png")
+  bulletImage=loadImage("Images/bullet.png")
+  hotAirBalloonImage=loadImage("Images/HotAirBalloon.png")
+  backgroundImage=loadImage("Images/background.png")
 }
 
 function setup() {
+  
   createCanvas(900,500);
 
-  mortar = createSprite(width-100,height-100);
-  mortar.addImage("Mortar1",mortarImage)
-  mortar.scale=1.75
+  
+
+  ground=createSprite(225,450,width*2,20)
+  ground.addImage("ground",backgroundImage)
+  
+
+  ground2=createSprite(675,450,width*2,20)
+  ground2.addImage("ground",backgroundImage)
+  
 
   cloudGroup=new Group()
   bulletGroup=new Group()
   floatingObjectGroup=new Group()
+  
+  mortar = createSprite(width-100,height-100);
+  mortar.addImage("Mortar1",mortarImage)
+  mortar.scale=0.6
+  mortar.setCollider("circle",100,-20,30)
 }
 
 function draw() {
@@ -29,16 +45,21 @@ function draw() {
   if (keyWentDown("space")){
     spawnBullet()
   }
-  mortar.pointTo(mouseX,mouseY)
+  if(mouseX<810&&mouseX>716&&mouseY>296&&mouseY<388){
+    mortar.pointTo(mouseX,mouseY)
+  }
+  mortar.debug=true
   drawSprites();
 }
 
 function spawnBullet(){
 
   var bullet = createSprite(mortar.x,mortar.y,17.5,17.5)
-  bullet.shapeColor="red"
-  bullet.velocityX=-3
-  bullet.velocityY=-3
+  bullet.addImage("bulletImage",bulletImage)
+  bullet.debug=true
+  bullet.setCollider("circle",-10,15,7.5)
+  bullet.velocityX=-8
+  bullet.velocityY=-8
   bulletGroup.add(bullet)
 }
 
@@ -49,6 +70,8 @@ function spawnFloatingObjects() {
   floatingObject1.velocityX = 10;
   floatingObject1.lifetime=900/10
   floatingObjectGroup.add(floatingObject1)
+  floatingObject1.addImage("HotAirBalloon",hotAirBalloonImage)
+  floatingObject1.scale=0.6
 }
 
 function spawnClouds(){
@@ -60,6 +83,7 @@ function spawnClouds(){
     cloud.lifetime=900/2.5
     cloudGroup.add(cloud)
     cloud.addImage(cloudImage)
+    cloud.scale=0.175
 
   }
   
